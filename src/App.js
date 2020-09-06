52612f26d3ca4f2f0c15eebbaaa050951d0e7311
Emiliano Gallardo <eagallardo@fi.uba.ar>
2020-09-05 21:22:55 -0300

    new: create new task and delete

--- src/App.js
+++ src/App.js
@@ -1,46 +1,97 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Column from "./components/column/Column";
 import logo from './assets/hr_logo.png';
 
-const board = [
-    {
-        title: 'Backlog',
-        tasks: [{
-            "id": 1,
-            "title": "Task 1",
-        }]
-    }, {
-        title: 'To Do',
-        tasks: []
-    },  {
-        title: 'Ongoing',
-        tasks: []
-    }, {
-        title: 'Done',
-        tasks: []
-    }
+const initialBoard = [
+  {
+    title: 'Backlog',
+    tasks: [{
+      title: "Task 1",
+    }]
+  }, {
+    title: 'To Do',
+    tasks: []
+  }, {
+    title: 'Ongoing',
+    tasks: []
+  }, {
+    title: 'Done',
+    tasks: []
+  }
 ]
 
 const App = () => {
+
+  const [newTask, setNewTask] = useState("");
+  const [board, setBoard] = useState(initialBoard);
+
+  const onNext = (columnId, taskId) => {
+    if(columnId > board.length) return null;
+
+    // TODO: Implement
+  }
+
+  const onBack = (columnId, taskId) => {
+    if(columnId <= 0) return null;
+
+    // TODO: Implement
+  }
+
+  const onDelete = (columnId, taskId) => {
+    const newBoard = [...board];
+    newBoard[columnId].tasks.splice(taskId, 1);
+    setBoard(newBoard);
+  }
+
+  const createTask = (name) => {
+    const newBoard = [...board];
+    newBoard[0].tasks.push({ title: name });
+    setBoard(newBoard);
+    setNewTask("");
+  }
+
+  const onChangeNewTask = event => {
+    setNewTask(event.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-              <img src={logo} alt="HR" className="App-header-logo"/>
-              <span className="App-header-title">Kanban Board</span>
+        <img src={logo} alt="HR" className="App-header-logo"/>
+        <span className="App-header-title">Kanban Board</span>
       </header>
       <div className="App-content container">
-          <div className="App-input">
-              <input className="App-input-text" type="text" name="name" placeholder="New task name" />
-              <input className="App-input-button" type="submit" value="Create task"/>
-          </div>
-          <div className="row">
-              {board && board.map(column =>
-                  <div className="col-sm-12 col-md-3 col-lg-3">
-                      <Column title={column.title} tasks={column.tasks}/>
-                  </div>
-              )}
-          </div>
+        <div className="App-input">
+          <input
+            className="App-input-text"
+            type="text"
+            name="name"
+            value={newTask}
+            placeholder="New task name"
+            onChange={onChangeNewTask}
+          />
+          <input
+            className="App-input-button"
+            disabled={!newTask || newTask === ""}
+            type="submit"
+            value="Create task"
+            onClick={() => createTask(newTask) }/>
+        </div>
+        <div className="row">
+          {board && board.map((column,index) =>
+            <div className="col-sm-12 col-md-3 col-lg-3">
+              <Column
+                id={index}
+                title={column.title}
+                tasks={column.tasks}
+                onBack={onBack}
+                onNext={onNext}
+                onDelete={onDelete}
+              />
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
--- src/components/column/Column.js
+++ src/components/column/Column.js
@@ -1,37 +1,44 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './column.css';
 import leftArrow from "../../assets/left-arrow.svg"
 import rightArrow from "../../assets/right-arrow.svg"
 import trash from "../../assets/trash.svg"
 
-const Column = ({ title, tasks }) => {
-    return (
-        <div className="column-container">
-            <div className="column-name">
-                <span>{ title }</span>
-            </div>
-            {(tasks && tasks.length > 0) && (
-                <div className="tasks-container">
-                    {tasks.map(task => (
-                        <Task title={task.title} />
-                    ))}
-                </div>
-            )}
+const Column = ({id, title, tasks, onBack, onNext, onDelete}) => {
+  return (
+    <div className="column-container">
+      <div className="column-name">
+        <span>{title}</span>
+      </div>
+      {(tasks && tasks.length > 0) && (
+        <div className="tasks-container">
+          {tasks.map((task, index) => (
+            <Task
+              id={index}
+              title={task.title}
+              columnId={id}
+              onBack={onBack}
+              onNext={onNext}
+              onDelete={onDelete}
+            />
+          ))}
         </div>
-    )
+      )}
+    </div>
+  )
 }
 
-const Task = ({ title }) => {
-    return (
-        <div className="task">
-                <span>{title}</span>
-                <div className="actions-container">
-                    <img src={leftArrow} alt="Back" className="action-icon" />
-                    <img src={rightArrow} alt="Next" className="action-icon" />
-                    <img src={trash} alt="Delete" className="action-icon" />
-                </div>
-        </div>
-    );
+const Task = ({id, title, columnId, onBack, onNext, onDelete}) => {
+  return (
+    <div className="task">
+      <span>{title}</span>
+      <div className="actions-container">
+        <img src={leftArrow} alt="Back" className="action-icon" onClick={() => onBack(id, columnId)}/>
+        <img src={rightArrow} alt="Next" className="action-icon" onClick={() => onNext(id, columnId)}/>
+        <img src={trash} alt="Delete" className="action-icon" onClick={() => onDelete(id, columnId)}/>
+      </div>
+    </div>
+  );
 }
 
 export default Column;
